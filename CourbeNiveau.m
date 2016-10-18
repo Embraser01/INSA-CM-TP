46bc01bd8cfab0dc1baaa0592094f4236322fe9f
unknown <gmarcelin@IF501-204-18.insa-lyon.fr>
2016-10-18 16:37:20 +0200

    Add courbeNiveau

---
+++ CourbeNiveau.m
@@ -0,0 +1,29 @@
+function [ texture ] = CourbeNiveau( terrain )
+%Genere une courbe d eniveau a partir des altitudes
+%   Detailed explanation goes here
+n = size(terrain);
+pas = 25;
+epsNoir =1.25;
+epsGris = 2;
+texture = ones(n(1),n(2),3);
+texture = uint8(texture);
+texture = texture * 240;
+    for i=1:n(1),
+        for j =1:n(2),
+            if( (abs(mod(terrain(i,j),pas)) < epsNoir  ) || (abs (mod(terrain(i,j),pas) - pas) < epsNoir ) ) 
+                texture(i,j,1) = 0;
+                texture(i,j,2) = 0;
+                texture(i,j,3) = 0;
+            elseif((abs(mod(terrain(i,j),pas)) < epsGris  ) || (abs (mod(terrain(i,j),pas) - pas) < epsGris ) ) 
+                texture(i,j,1) = 130;
+                texture(i,j,2) = 130;
+                texture(i,j,3) = 130;
+            end;
+        end
+    end
+    
+    texture = uint8(texture);
+    
+    imwrite(texture,'courbeNiveau.png');
+
+end
